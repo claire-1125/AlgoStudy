b76bb012ebca3fedb473a2ef7d4df67b046e0e0b
JeongBeen Seo <70363711+claire-1125@users.noreply.github.com>
2025-01-22 11:42:51 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 233ms, Memory: 27MB, Status: Passed - CodeTree

--- 250122/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 250122/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -2,14 +2,10 @@ K, N = map(int, input().split())
 
 # Write your code here!
 
-# Dynamic Programming ~ Divide and Conquer
-# 방법: 재귀, 순회
-
 # 재귀: 종료 조건 + 재귀 호출
 # 순열, 조합 만들 시 재귀함수 사용 가능
 
-
-answer = []
+answer = []  # 결과를 담는 리스트
 
 def print_answer():
     for elem in answer:
@@ -18,15 +14,19 @@ def print_answer():
 
 
 def choose(curr_num):
+    # curr_num: 재귀 횟수 카운팅 (1부터 시작)
+    # 'K진수 중 택1'을 N번 진행
+
     # 종료 조건
-    if curr_num == N+1:
-        print_answer()
+    if curr_num == N+1:  # curr_num은 1부터 시작, N번만큼 재귀할 때는 N+1이 됨.
+        print_answer()  # 함수 호출 대신 밖에서 출력하려 하면 answer는 이미 비워져 있음.
         return
 
     # 재귀 호출
-    for i in range(1,K+1):
+    for i in range(1,K+1):  # K진수에 대해서 택 1 진행  
+        # 재귀를 계속 타다 맨 안 쪽에서 print_answer() 호출 뒤 맨 마지막 원소 삭제
         answer.append(i)
-        choose(curr_num + 1)
-        answer.pop()
+        choose(curr_num+1) 
+        answer.pop()  # 맨 뒤에 있는 원소 빼내기만 함. (출력 X)
 
 choose(1)
